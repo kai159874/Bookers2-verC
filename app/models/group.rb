43349bf2ed2432dc97c793be92638383f364eab7
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-03-29 02:30:43 +0000

    [modify] group init

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,9 @@ class Group < ApplicationRecord
   has_many :group_users
   has_one_attached :group_image
 
+  validates :name, presence: true
+  validates :introduction, presence: true
+
   def get_profile_image
     (group_image.attached?) ? group_image : 'no_image.jpg'
   end
